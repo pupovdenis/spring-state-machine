2c6d981cc657dd771d7928a4d5bc05a12b6884a2
dpupov <dpupov@sitronics.com>
2025-05-08 17:05:38 +0500

    Initial commit

---
+++ src/test/java/ru/pupov/spring_state_machine/SpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pupov.spring_state_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
