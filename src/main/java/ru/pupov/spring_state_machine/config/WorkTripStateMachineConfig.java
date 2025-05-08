c08a637bc2533579f3ce021b7994720f9791e17b
dpupov <dpupov@sitronics.com>
2025-05-08 19:42:20 +0500

    add action and listener

---
+++ src/main/java/ru/pupov/spring_state_machine/config/WorkTripStateMachineConfig.java
@@ -0,0 +1,99 @@
+package ru.pupov.spring_state_machine.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.action.Action;
+import org.springframework.statemachine.config.EnableStateMachine;
+import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListener;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.transition.Transition;
+import ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS;
+import ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES;
+
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.ADD_CHECK_IN_TICKETS;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.ADD_CHECK_OUT_TICKETS;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.ADD_REPORTING_DOCUMENTS;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.ADD_SPECIFICATION;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.CLOSE_WORK_TRIP;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.START_ARRIVAL_ROAD;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.START_DEPARTURE_ROAD;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.START_WORKING;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.ARRIVAL_ROAD_FACT;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.CLOSED;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.CREATED;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.DEPARTURE_ROAD_FACT;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.DOCUMENTS_READY;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.FIXED_CHECK_IN_TICKETS;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.FIXED_CHECK_OUT_TICKETS;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.IN_PROGRESS;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.SPECIFIED;
+
+@Configuration
+@EnableStateMachine
+@Slf4j
+public class WorkTripStateMachineConfig extends EnumStateMachineConfigurerAdapter<WORK_TRIP_STATES, WORK_TRIP_EVENTS> {
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<WORK_TRIP_STATES, WORK_TRIP_EVENTS> config) throws Exception {
+        config.withConfiguration()
+                .autoStartup(true)
+                .listener(listener());
+    }
+
+    @Override
+    public void configure(StateMachineStateConfigurer<WORK_TRIP_STATES, WORK_TRIP_EVENTS> states) throws Exception {
+        System.out.println("Registering states...");
+        states.withStates()
+                .initial(CREATED, notifyOperator())
+                //.states(EnumSet.allOf(WORK_TRIP_STATES.class));
+                .state(SPECIFIED, context -> log.info("need check-in tickets"))
+                .state(FIXED_CHECK_IN_TICKETS)
+                .state(DEPARTURE_ROAD_FACT)
+                .state(IN_PROGRESS, context -> log.info("need check-out tickets"))
+                .state(FIXED_CHECK_OUT_TICKETS)
+                .state(ARRIVAL_ROAD_FACT, context -> log.info("need documents"))
+                .state(DOCUMENTS_READY)
+                .end(CLOSED);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<WORK_TRIP_STATES, WORK_TRIP_EVENTS> transitions) throws Exception {
+        transitions.withExternal().source(CREATED).target(SPECIFIED).event(ADD_SPECIFICATION)
+                .and().withExternal().source(SPECIFIED).target(FIXED_CHECK_IN_TICKETS).event(ADD_CHECK_IN_TICKETS)
+                .and().withExternal().source(FIXED_CHECK_IN_TICKETS).target(DEPARTURE_ROAD_FACT).event(START_DEPARTURE_ROAD)
+                .and().withExternal().source(DEPARTURE_ROAD_FACT).target(IN_PROGRESS).event(START_WORKING).action(context -> log.info("working"))
+                .and().withExternal().source(IN_PROGRESS).target(FIXED_CHECK_OUT_TICKETS).event(ADD_CHECK_OUT_TICKETS)
+                .and().withExternal().source(FIXED_CHECK_OUT_TICKETS).target(ARRIVAL_ROAD_FACT).event(START_ARRIVAL_ROAD)
+                .and().withExternal().source(ARRIVAL_ROAD_FACT).target(DOCUMENTS_READY).event(ADD_REPORTING_DOCUMENTS).action(context -> log.info("notify to send documents"))
+                .and().withExternal().source(DOCUMENTS_READY).target(CLOSED).event(CLOSE_WORK_TRIP);
+    }
+    private Action<WORK_TRIP_STATES, WORK_TRIP_EVENTS> notifyOperator() {
+        return context -> log.info("need documents");
+    }
+
+    private StateMachineListener<WORK_TRIP_STATES, WORK_TRIP_EVENTS> listener() {
+        return new StateMachineListenerAdapter<>() {
+            @Override
+            public void transition(Transition<WORK_TRIP_STATES, WORK_TRIP_EVENTS> transition) {
+                log.info("change {} to {}",
+                        ofNullableState(transition.getSource()),
+                        ofNullableState(transition.getTarget()));
+            }
+
+            @Override
+            public void eventNotAccepted(Message<WORK_TRIP_EVENTS> event) {
+                log.error("not accepted: {}", event.getPayload());
+            }
+
+            //duct tape
+            private Object ofNullableState(State<WORK_TRIP_STATES, WORK_TRIP_EVENTS> s) {
+                return s != null ? s.getId() : null;
+            }
+        };
+    }
+}
--- src/test/java/ru/pupov/spring_state_machine/SpringStateMachineApplicationTests.java
+++ src/test/java/ru/pupov/spring_state_machine/SpringStateMachineApplicationTests.java
@@ -1,12 +1,15 @@
 package ru.pupov.spring_state_machine;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
+import reactor.core.publisher.Mono;
 import ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS;
 import ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES;
 
@@ -18,36 +21,59 @@ import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.CLOSE_
 import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.START_ARRIVAL_ROAD;
 import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.START_DEPARTURE_ROAD;
 import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_EVENTS.START_WORKING;
-import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.*;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.CLOSED;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.CREATED;
+import static ru.pupov.spring_state_machine.statemachine.WORK_TRIP_STATES.SPECIFIED;
 
 @SpringBootTest
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class SpringStateMachineApplicationTests {
 
-	@Autowired
-	private StateMachine<WORK_TRIP_STATES, WORK_TRIP_EVENTS> stateMachine;
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    public StateMachine<WORK_TRIP_STATES, WORK_TRIP_EVENTS> stateMachine;
 
-	@Test
-	void testInit() {
-		Assertions.assertNotNull(stateMachine);
-		Assertions.assertEquals(stateMachine.getState().getId(), CREATED);
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	void greenWay() {
-		//sendEvent(Mono.just(MessageBuilder.withPayload(MyEvent.MY_EVENT).build())).subscribe()
-		stateMachine.sendEvent(ADD_SPECIFICATION);
-		stateMachine.sendEvent(ADD_CHECK_IN_TICKETS);
-		stateMachine.sendEvent(START_DEPARTURE_ROAD);
-		stateMachine.sendEvent(START_WORKING);
-		stateMachine.sendEvent(ADD_CHECK_OUT_TICKETS);
-		stateMachine.sendEvent(START_ARRIVAL_ROAD);
-		stateMachine.sendEvent(ADD_REPORTING_DOCUMENTS);
-		stateMachine.sendEvent(CLOSE_WORK_TRIP);
-		Assertions.assertEquals(stateMachine.getState().getId(), CREATED);
-	}
+    @BeforeEach
+    void setUp() {
+        stateMachine.startReactively().subscribe();
+    }
+
+    @Test
+    void testInit() {
+        Assertions.assertNotNull(stateMachine);
+        Assertions.assertEquals(stateMachine.getState().getId(), CREATED);
+    }
+
+    @Test
+    void testGreenWay() {
+        //sendEvent(Mono.just(MessageBuilder.withPayload(MyEvent.MY_EVENT).build())).subscribe()
+        sendEvent(stateMachine, ADD_SPECIFICATION);
+        sendEvent(stateMachine, ADD_CHECK_IN_TICKETS);
+        sendEvent(stateMachine, START_DEPARTURE_ROAD);
+        sendEvent(stateMachine, START_WORKING);
+        sendEvent(stateMachine, ADD_CHECK_OUT_TICKETS);
+        sendEvent(stateMachine, START_ARRIVAL_ROAD);
+        sendEvent(stateMachine, ADD_REPORTING_DOCUMENTS);
+        sendEvent(stateMachine, CLOSE_WORK_TRIP);
+        Assertions.assertEquals(CLOSED, stateMachine.getState().getId());
+    }
+
+    @Test
+    void testWrongWay() {
+        sendEvent(stateMachine, ADD_SPECIFICATION);
+        sendEvent(stateMachine, START_WORKING);
+        Assertions.assertEquals(SPECIFIED, stateMachine.getState().getId());
+    }
+
+    private void sendEvent(StateMachine<WORK_TRIP_STATES, WORK_TRIP_EVENTS> stateMachine, WORK_TRIP_EVENTS event) {
+        stateMachine.sendEvent(
+                Mono.just(MessageBuilder
+                        .withPayload(event)
+                        .build())
+        ).subscribe();
+    }
 }
